ca7185ab3454e67a67e44f4991b7863193ed900b
Murat Sever <murat-sever@live.com>
2024-02-27 23:58:50 +0300

    Deploy website - based on 94e03046d2771e96be8b3dda3fe6e18585e769f9

---
+++ assets/js/19e8975a.cd17d30c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[3596],{3209:e=>{e.exports=JSON.parse('[{"label":"hello","permalink":"/outreach/tags/hello","count":1}]')}}]);
---
+++ assets/js/c87c1c37.f8c16299.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_website=self.webpackChunkmy_website||[]).push([[8697],{9161:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"Welcome","permalink":"/outreach/welcome"}]}')}}]);
